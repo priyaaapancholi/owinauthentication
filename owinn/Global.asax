16db9fbbf1c85127f881b56b1aa7850ee1039237
Priya <priya.pancholi@promactinfo.com>
2016-07-22 15:16:04 +0530

    Add project files.

---
+++ owinn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="owinn.MvcApplication" Language="C#" %>
---
+++ owinn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(owinn.Startup))]
+namespace owinn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
